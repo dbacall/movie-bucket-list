f04b4bf2eed6c9854eeb4ab1b236e891164f07ef
dbacall <dbacall@hotmail.co.uk>
2020-03-24 11:31:58 +0000

    feat: add login route to users.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://dbacall:Marzipan17@moviebucketlist-hzwtr.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://dbacall:Marzipan17@moviebucketlist-hzwtr.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -37,3 +37,49 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+router.post("login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+module.exports = router;
