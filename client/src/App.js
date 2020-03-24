2d84965e3cec11c6e2abc620ebe3a34e3d65e53b
dbacall <dbacall@hotmail.co.uk>
2020-03-24 12:52:27 +0000

    chore: set up redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from "./components/movie-list";
 import Navbar from "./components/navbar";
+import Register from "./components/register";
+import Login from "./components/login";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <MovieList />
-        </React.Fragment>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={MovieList} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
