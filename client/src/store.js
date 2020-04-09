e2e07b6631af1f79b2cfe3f5dbccd05d235ce611
dbacall <dbacall@hotmail.co.uk>
2020-04-09 14:24:14 +0100

    change redux store.js for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -8,12 +8,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
-      compose
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
