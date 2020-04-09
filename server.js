46b6674ce0055088c92b6ff17e30bfc71690e2b5
dbacall <dbacall@hotmail.co.uk>
2020-04-09 13:14:39 +0100

    chore: update server.js for deployment

--- server.js
+++ server.js
@@ -40,15 +40,15 @@ app.use("/users", users);
 
 app.use("/movies", movies);
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 
