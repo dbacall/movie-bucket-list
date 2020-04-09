61055a66b8046232671dcc267c8a749b16f85903
dbacall <dbacall@hotmail.co.uk>
2020-04-09 12:32:43 +0100

    chore: update server.js for deployment to heroku

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const movies = require("./routes/api/movies");
+const path = require("path");
 
 const db = require("./config/keys").mongoURI;
-const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
@@ -25,10 +25,10 @@ app.use(bodyParser.json());
 mongoose
   .connect(db, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   })
   .then(() => console.log("DB Connected!"))
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
 
@@ -39,5 +39,17 @@ require("./config/passport")(passport);
 app.use("/users", users);
 
 app.use("/movies", movies);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
